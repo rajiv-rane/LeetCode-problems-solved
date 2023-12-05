435b2f2890fe145d51f1eef9859c3c28dedb4a34
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2023-12-05 20:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 6.2 MB (73.09%) - LeetSync

---
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        return n-1;
+    //     int count=2;
+    //     if(n==1){return 1;}
+    //     else if(n%2==0){
+    //         //even
+    //         while(n>0){
+    //             n=n/2;
+    //             count=count+n;
+    //         }
+    //     }
+    //     else{//odd
+    //         while(n>0){
+    //             n=(n-1)/2;
+    //             count=count+n;
+    //         }
+    //     }
+    //     return count;
+    }
+};
