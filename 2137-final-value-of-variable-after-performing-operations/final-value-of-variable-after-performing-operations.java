df326366ad9f6d7edc4ab1dc8ef7c44f8fc40242
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-03-09 22:50:05 +0530

    Time: 1 ms (75.05%) | Memory: 42.8 MB (33.58%) - LeetSync

---
+++ 2137-final-value-of-variable-after-performing-operations/final-value-of-variable-after-performing-operations.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int x=0;
+        for(String s: operations){
+            if(s.equals("--X") || s.equals("X--"))
+                x--;
+            if(s.equals("++X") || s.equals("X++"))
+                x++;
+        }
+        return x;
+    }
+}
