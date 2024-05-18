0b6f30bfd88dbafc5e13884f610b752b6a596a28
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-05-18 18:47:27 +0530

    Time: 1 ms (71.12%) | Memory: 45.8 MB (67.28%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,35 +1,25 @@
 class Solution {
     public int trap(int[] height) {
-        // for every gate find the largest gate closest to it
-        int i,j,k;
+        int i,j,ans=0;
         int n=height.length;
-        int leftmax=0,rightmax=0;
-        int [] left = new int[n];
-        Arrays.fill(left, 0);
-        int [] right = new int[n];
-        Arrays.fill(right, 0);
-        left[0]=height[0];
-        right[n-1]=height[n-1];
-        int [] water = new int[n];
-        Arrays.fill(water, 0);
-        int ans=0;
-        
-        // left max height
+        int leftmax[]=new int [n];
+        int rightmax[]=new int [n];
+        leftmax[0]=height[0];
+        rightmax[n-1]=height[n-1];
+
         for(i=1;i<n;i++){
-            left[i]=Math.max(left[i-1],height[i]);
+            leftmax[i]=Math.max(leftmax[i-1],height[i]);
         }
-        // right max ht
         for(i=n-2;i>=0;i--){
-            right[i]=Math.max(right[i+1],height[i]);
+            rightmax[i]=Math.max(rightmax[i+1],height[i]);
         }
-
-        // water stored @i=min(Leftmax,rightmax)-gate[i]
-
+        int water=0;
         for(i=0;i<n;i++){
-            water[i]=Math.min(left[i],right[i])-height[i];
-            // if(water[i]>0)
-                ans=ans+water[i];
+            water=Math.min(leftmax[i],rightmax[i])-height[i];
+            // if(water>0)
+                ans=ans+water;
         }
         return ans;
+        
     }
 }
