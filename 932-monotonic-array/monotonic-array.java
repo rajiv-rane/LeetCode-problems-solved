59ea057ccb81280f19f8ec327a18da9e14a2f00b
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-05-25 11:47:00 +0530

    Time: 1 ms (99.64%) | Memory: 59.1 MB (53.07%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -1,66 +1,19 @@
 class Solution {
+    public boolean is_inc(int[] nums){
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]>nums[i])
+                return false;
+        }
+        return true;
+    }
+    public boolean is_dec(int[] nums){
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]<nums[i])
+                return false;
+        }
+        return true;
+    }
     public boolean isMonotonic(int[] nums) {
-        // HashMap<Integer,Integer>map=new HashMap<>();
-        // for(int i:nums){
-        //     if(map.containsKey(i)){
-        //         map.put(i,map.get(i)+1);
-        //     }
-        //     else{
-        //         map.put(i,1);
-        //     }
-        // }
-        int n=nums.length;
-        int i,j;
-        boolean flag=true;
-        for(i=1;i<n;i++){
-            if(nums[i-1]<=nums[i])
-                continue;
-            else{
-                flag=false;
-                break;
-            }       
-        }
-        if(flag==true)
-            return true;
-        flag=true;
-        for(i=1;i<n;i++){
-            if(nums[i-1]>=nums[i])
-                continue;
-            else
-                flag=false;
-        }
-        return flag;
-
-
-        // int revArray[]=new int[n];
-        // int copyArray[]=new int[n]
-        // j=0;
-        // for(i=n-1;i>=0;i--){
-        //     revArray[j]=nums[i];
-        //     copyArray[i]=nums[i];
-        //     j++;
-        // }
-        // Arrays.reverse()
-
-        // int flag=0;
-        // for(i=1;i<n;i++){
-        //     if(nums[i-1]<=nums[i])  
-        //         flag=1;
-        //     else
-        //         flag=0;
-        // }
-        // for(i=1;i<n;i++){
-        //     if(nums[i]<=nums[i-1])
-        //         flag=1;
-        //     else
-        //         flag=0;
-        // }
-
-        // if(flag==1)
-        //     return true;
-        // else 
-        //     return false;
-        
-        
+        return (is_inc(nums)|| is_dec(nums));
     }
 }
