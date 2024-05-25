46212dc294c45f2126176ed4b84b0a492a1b957d
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-05-25 11:43:25 +0530

    Time: 2 ms (68.43%) | Memory: 59.5 MB (21.42%) - LeetSync

--- 932-monotonic-array/monotonic-array.java
+++ 932-monotonic-array/monotonic-array.java
@@ -32,34 +32,7 @@ class Solution {
         return flag;
 
 
-        // int revArray[]=new int[n];
-        // int copyArray[]=new int[n]
-        // j=0;
-        // for(i=n-1;i>=0;i--){
-        //     revArray[j]=nums[i];
-        //     copyArray[i]=nums[i];
-        //     j++;
-        // }
-        // Arrays.reverse()
-
-        // int flag=0;
-        // for(i=1;i<n;i++){
-        //     if(nums[i-1]<=nums[i])  
-        //         flag=1;
-        //     else
-        //         flag=0;
-        // }
-        // for(i=1;i<n;i++){
-        //     if(nums[i]<=nums[i-1])
-        //         flag=1;
-        //     else
-        //         flag=0;
-        // }
-
-        // if(flag==1)
-        //     return true;
-        // else 
-        //     return false;
+        
         
         
     }
