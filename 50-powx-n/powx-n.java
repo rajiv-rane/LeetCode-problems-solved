1b70d7058f650e0b6aeea73f0074ea905ffa829b
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-05-26 00:26:25 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (15.97%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,22 +1,19 @@
 class Solution {
     public double myPow(double x, int n) {
-        double result=1;
-        int N = Math.abs(n);
-        if(n == Integer.MIN_VALUE) {
-            N = Integer.MAX_VALUE - 1;
-        }
+        int N=Math.abs(n);
+        if(N==Integer.MIN_VALUE)
+            N=Integer.MAX_VALUE-1;
+        double res=1;
         while(N>0){
-            // it will perfrom a step for x^1 
             if(N%2==1){
-                //if power is odd then that extra x^1 should be multiplied to result
-                result=result*x;
+                res=res*x;
             }
             x=x*x;
             N=N/2;
         }
         if(n<0)
-            return 1/result;
+            return 1/res;
         else
-            return result;
+            return res;
     }
 }
