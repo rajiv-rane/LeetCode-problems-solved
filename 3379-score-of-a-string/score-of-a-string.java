f91a870e5cd5af8e5adf9de0b06baec484f1ecc8
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-05-27 23:11:12 +0530

    Time: 1 ms (99.87%) | Memory: 41.8 MB (76.96%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.java
+++ 3379-score-of-a-string/score-of-a-string.java
@@ -4,6 +4,8 @@ class Solution {
         int n=str.length;
         int score=0;
         // int j=1;
+        if(n==0)
+            return 0;
         for(int i=0;i<n-1;i++){
             
                 score=score+Math.abs((int)str[i]-(int)str[i+1]);
