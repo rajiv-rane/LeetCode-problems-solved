82a157ce4f40c7896a9a9d64e4c1ebb67ec004f7
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-06-11 22:14:26 +0530

    Time: 1 ms (99.51%) | Memory: 56.4 MB (95.25%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,51 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        //using kadane's algorithm
-        /*We initialize current_max variable which denotes the max sum of subarrays achieved until index i */
-        /*initialize current_max=global_max=nums[0]
-        since nums[0] is the max subarray sum ending with element nums[0]*/
-        /*we use another variable global_max =nums[0]*/
-        int maxSum=Integer.MIN_VALUE;
-        int sum=0;
+        int globalsum=Integer.MIN_VALUE;
+        int currsum=0;
         for(int i=0;i<nums.length;i++){
-            /*carry forward the sum if the current sum is positive*/
-            sum=sum+nums[i];
-            maxSum=Math.max(sum,maxSum);
-            /*if the current sum is negative then no need to include it 
-            while carrying it forward*/
-            if(sum<0){
-                sum=0;
-                /*if sum is -ve => rest sum to 0 
-                since we don't need -ve resultant to be added to sum*/ 
-            }
+            if(currsum<0)
+                currsum=0;
+            currsum+=nums[i];
+            globalsum=Math.max(currsum,globalsum);
+
         }
-        return maxSum;
+        return globalsum;
     }
 }
-
-// class Solution {
-//     public int maxSubArray(int[] nums) {
-//         int i,j;
-//         int n=nums.length;
-//         int pf[]=new int [n];
-//         pf[0]=nums[0];
-//         for(i=1;i<n;i++){
-//             pf[i]=pf[i-1]+nums[i];
-//         }
-        
-//         int sum=0;
-//         int ans=nums[0];
-//         for(i=0;i<n;i++){
-//             for(j=i;j<n;j++){
-//                 if(i==0){
-//                     sum=pf[j];
-//                 }
-//                 else{
-//                     sum=pf[j]-pf[i-1];
-//                 }
-//                 ans=Math.max(ans,sum);
-//             }
-//         }
-//         return ans;
-//     }
-// }
