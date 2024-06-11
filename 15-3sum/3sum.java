2c311b49c051cbc196b889089f0f3b28ecce2a8a
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-06-11 22:44:58 +0530

    Time: 929 ms (8.81%) | Memory: 51.9 MB (44.56%) - LeetSync

---
+++ 15-3sum/3sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        // The Arrays.asList() in Java can accept an array/elements as an argument and convert it to a list.
+        if(nums==null || nums.length<3)
+            return new ArrayList<>();
+        Arrays.sort(nums);
+        Set<List<Integer>>ans=new HashSet<>();
+
+        for(int i=0;i<nums.length-2;i++){
+            int left=i+1;
+            int right=nums.length-1;
+            //2 pointers
+            while(left<right){
+                int sum=nums[left]+nums[right]+nums[i];
+                if(sum==0){
+                    ans.add(Arrays.asList(nums[i],nums[left],nums[right]));
+                    left++;right--;
+                }
+                else if(sum<0){
+                    left++;
+                }
+                else{
+                    right--;
+                }
+            }
+        }
+        return new ArrayList<>(ans);
+    }
+}
