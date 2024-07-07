cefee4bdb83fb928a482ea3d2cbacd945f0d531b
Rajiv Rane <120100767+rajiv-rane@users.noreply.github.com>
2024-07-07 11:17:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.2 MB (36.25%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,28 +1,18 @@
-import java.util.*;
 class Solution {
-    HashMap<Integer,Integer>map;
-    Solution(){
-        map=new HashMap<>();
-        System.out.println("hi");
-    }
-    int test(int n,HashMap map){
-        if(map.containsKey(n))
-            return (int)map.get(n);
-        
-        else
-        {
-            map.put(n,climbStairs(n-1)+climbStairs(n-2));
-            return climbStairs(n-1)+climbStairs(n-2);
-        }
-    }
-    // sample();
     public int climbStairs(int n) {
-         //map=new HashMap<>();
-
-        map.put(2,2);
-        map.put(1,1);
-        return test(n,map);
-            
-            
+        if(n==1)
+            return 1;
+        if(n==2)
+            return 2;
+        if(n==3)
+            return 3;
+        int dp[]=new int[n+1];
+        dp[1]=1;
+        dp[2]=2;
+        dp[3]=3;
+        for(int i=4;i<n+1;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 }
