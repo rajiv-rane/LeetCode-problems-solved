d14e82805785c9e5c30dcadd8796c70537f7cf2c
Rajiv <120100767+rajiv-rane@users.noreply.github.com>
2025-02-20 08:50:04 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (81.42%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,21 +1,20 @@
 class Solution {
 
-    public void helper(int[] nums,List<List<Integer>>output,List<Integer>ans,int index){
-        output.add(new ArrayList<>(ans));
+    public void backtrack(int[] nums,int index,List<List<Integer>>output,List<Integer>current){
+        output.add(new ArrayList<>(current));
         for(int i=index;i<nums.length;i++){
-            ans.add(nums[i]);
-            helper(nums,output,ans,i+1);//call the next index
-            ans.remove(ans.size()-1);
+            current.add(nums[i]);
+            backtrack(nums,i+1,output,current);
+            current.remove(current.size()-1);
         }
+        return;
     }
 
     public List<List<Integer>> subsets(int[] nums) {
-        // for every nums[i] we got 2 choices
-        // 1. either pick the element 2. either ignore the element
-// so append and remove the element from a intermediate list to mock the above 2 steps
         List<List<Integer>>output=new ArrayList<>();
-        List<Integer>ans=new ArrayList<>();
-        helper(nums,output,ans,0);
+        List<Integer>current=new ArrayList<>();
+        backtrack(nums,0,output,current);
         return output;
+        
     }
 }
