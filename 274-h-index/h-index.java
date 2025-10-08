573714e22c09610f710411463331bc85adbbe2a8
Rajiv <120100767+rajiv-rane@users.noreply.github.com>
2025-10-08 12:42:11 +0530

    Time: 2 ms (68.96%) | Memory: 42 MB (20.58%) - LeetSync

---
+++ 274-h-index/h-index.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        // we sort the array and then check from the end
+        // if the count (citations[i] value)==index from end then it's a H index.
+        // if the condition of the loop fails then the i-1 element is the H index
+
+        Arrays.sort(citations);
+        int n=citations.length;
+        int i;
+        for(i=1;i<=n;i++){
+            if(citations[n-i]<i)
+            break;
+        }
+        return i-1;
+    }
+}
