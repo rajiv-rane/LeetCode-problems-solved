ea210f1e878451cb5b1f4d7bbcf8432473887328
Rajiv <120100767+rajiv-rane@users.noreply.github.com>
2026-02-25 13:09:42 +0530

    Time: 1 ms (99.84%) | Memory: 55.8 MB (31.65%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,21 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        // Boyer–Moore
-        int result=0;
+        //if we delete 2 distinct entries , then majority element won't change
+        int n=nums.length;
+        int ele,freq,i;
         int count=0;
-        for(int i:nums){
-            if(count==0){
-                result=i;
+        ele=nums[0];
+        freq=1;
+        for(i=1;i<n;i++){
+            if(freq==0){
+                //update prev element to current elemet
+                ele=nums[i];
+                freq=1;
             }
-            // Algorithm begins here
-
-            if(i==result){
-                count++;
+            else if(ele==nums[i]){
+                freq++;
             }
             else{
-                count--;
+                freq--;
             }
         }
-        return result;
+        return ele; //returning since Q says 1 majority elements always exists
+        //linear search the ele in arr , if count >N/2 then it's majority else 
+        //no majority element exists'
+
+        // for(i=0;i<n;i++){
+        //     if(nums[i]==ele){
+        //         count++;
+        //     }
+        // }
+        // if(count>n/2){return ele;}
     }
 }
